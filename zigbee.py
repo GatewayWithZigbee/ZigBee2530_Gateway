34f18cbd9a3205f233e98903a6192ad88c27c117
isaccmarting <isaccmarting@gmail.com>
2017-03-30 20:58:47 +0800

    zigbee.py: transport information between two ZigBees.

--- zigbee.py
+++ zigbee.py
@@ -5,7 +5,7 @@ import re
 
 ser = serial.Serial("/dev/ttyAMA0", 115200)
 
-srcAddr = raw_input("Please input the address:")
+dstAddr = raw_input("Please input the address:")
 ser.write('AT+GETADDR')
 time.sleep(0.1)
 while True:
@@ -13,12 +13,12 @@ while True:
 	if count != 0:
 	    myAddr = ser.read(count)
 	    myAddr = re.findall("0x[0-9A-F]+", myAddr)
-            myAddr = myAddr[0][2:]
+        myAddr = myAddr[0][2:]
 	    print 'My address is %s' % myAddr
 	    ser.flushInput()
-            break
+        break
 
-command = 'O2M 2 %s %s Hello, world!' % (myAddr, srcAddr)
+command = 'O2M 2 %s %s Hello, world!' % (myAddr, dstAddr)
 ser.write(command)
 time.sleep(0.1)
 while True:
@@ -27,7 +27,7 @@ while True:
 	    myAddr = ser.read(count)
 	    print myAddr
 	    ser.flushInput()
-            break
+        break
 
 ser.close()
 
